4b3b112f1155b32897f6c499664703df578bda38
ElenaIf <ivankina.es@gmail.com>
2021-03-09 17:25:13 +0200

    added images of pokemons

--- Day11Pokemons/main.js
+++ Day11Pokemons/main.js
@@ -1,40 +1,40 @@
-(function () {
-  document.addEventListener("DOMContentLoaded", ExecuteScript);
-
-  /* const users = [
-    {
-      name: {
-        title: "Mr",
-        first: "Alexander",
-        last: "Kristensen",
-      },
-      email: "alexander.kristensen@example.com",
-      phone: "98708762",
-      picture: {
-        large: "https://randomuser.me/api/portraits/men/8.jpg",
-        medium: "https://randomuser.me/api/portraits/med/men/8.jpg",
-        thumbnail: "https://randomuser.me/api/portraits/thumb/men/8.jpg",
-      },
-    },
-  ]; */
-
-  function ExecuteScript() {
-    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000")
-      .then((resp) => resp.json())
-      .then((json) => {
-        json.results.forEach(addPokemon);
+(function startUpload() {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=5")
+    .then((response) => {
+      return response.json();
+    })
+    .then((allPokemons) => {
+      allPokemons.results.forEach((pokemon) => {
+        fetchPokemonData(pokemon);
       });
-  }
-
-  function addPokemon(pokemon) {
-    const newEl = document.createElement("div");
-    newEl.className = "pokemon";
-    newEl.innerHTML = `
-        <div>${pokemon.name}</div>
-        
-        <div>  <a href="${pokemon.url}">${pokemon.url}</a></div>
-       
-    `;
-    document.querySelector(".pokemonContainer").appendChild(newEl);
-  }
+    });
 })();
+
+function fetchPokemonData(pokemon) {
+  let url = pokemon.url;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((pokeData) => {
+      addPokemon(pokeData);
+    });
+}
+
+function addPokemon(pokeData) {
+  const newEl = document.createElement("div");
+  newEl.className = "pokemon";
+  newEl.onclick = printHello;
+  newEl.id = `pokemon${pokeData.id}`;
+  newEl.innerHTML = `<div>${pokeData.name}</div>
+  <div>
+  <img
+    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeData.id}.png"
+  />
+</div>`;
+  document.querySelector("#pokemonContainer").appendChild(newEl);
+}
+
+function printHello(fetchPokemonData) {
+  console.log("Hello");
+}
