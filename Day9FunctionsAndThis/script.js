3ee88aff4e933257bd163a6958d6f4b0fe98278d
ElenaIf <ivankina.es@gmail.com>
2021-04-05 13:07:01 +0300

    fixed table sorting exercise

--- Day9FunctionsAndThis/script.js
+++ Day9FunctionsAndThis/script.js
@@ -1,29 +1,29 @@
 const getUsers = () => [
-  {
-    studentNo: 123,
-    name: "harry",
-    age: 15,
-  },
-  {
-    studentNo: 234,
-    name: "Ron",
-    age: 16,
-  },
-  {
-    studentNo: 135,
-    name: "Hermione",
-    age: 17,
-  },
-  {
-    studentNo: -1,
-    name: "Snape",
-    age: 55,
-  },
-  {
-    studentNo: -1,
-    name: "Hagrid",
-    age: 65,
-  },
+	{
+		studentNo: 123,
+		name: "harry",
+		age: 15,
+	},
+	{
+		studentNo: 234,
+		name: "Ron",
+		age: 16,
+	},
+	{
+		studentNo: 135,
+		name: "Hermione",
+		age: 17,
+	},
+	{
+		studentNo: -1,
+		name: "Snape",
+		age: 55,
+	},
+	{
+		studentNo: -1,
+		name: "Hagrid",
+		age: 65,
+	},
 ];
 
 const users = getUsers();
@@ -32,81 +32,81 @@ const table = document.querySelector("table");
 const tableBody = document.querySelector("tbody");
 const button = document.querySelector("#ageSorting");
 
-const executeScript = () => {
-  let newRow = document.createElement("tr");
-  newRow.innerHTML = `<td>123</td>
-    <td>Test</td>
-    <td>999</td>
-    <td>Student</td>`;
-  tableBody.appendChild(newRow);
-};
+// const executeScript = () => {
+// 	let newRow = document.createElement("tr");
+// 	newRow.innerHTML = `<td>123</td>
+//     <td>Test</td>
+//     <td>999</td>
+//     <td>Student</td>`;
+// 	tableBody.appendChild(newRow);
+// };
 
 const addUser = (userInfo) => {
-  let newRow1 = document.createElement("tr");
-  let isStaff = "";
+	let newRow1 = document.createElement("tr");
+	let isStaff = "";
 
-  if (userInfo.studentNo >= 0) {
-    isStaff = "Student";
-  } else {
-    isStaff = "Staff";
-  }
+	if (userInfo.studentNo >= 0) {
+		isStaff = "Student";
+	} else {
+		isStaff = "Staff";
+	}
 
-  newRow1.innerHTML = `<td>${userInfo.studentNo}</td>
+	newRow1.innerHTML = `<td>${userInfo.studentNo}</td>
     <td>${userInfo.name}</td>
     <td>${userInfo.age}</td>
     <td>${isStaff}</td>`;
-  tableBody.appendChild(newRow1);
+	tableBody.appendChild(newRow1);
 };
 
 users.forEach(addUser);
 
 const getOldest = (userObject) => {
-  const oldest = userObject.sort((a, b) => (a.age > b.age ? -1 : 1));
-  return oldest[0].name;
+	const oldest = userObject.sort((a, b) => (a.age > b.age ? -1 : 1));
+	return oldest[0].name;
 };
 
 document.querySelector("#oldest").textContent = `The oldest on the list is ${getOldest(users)}`;
 
-document.addEventListener("DOMContentLoaded", executeScript);
+// document.addEventListener("DOMContentLoaded", executeScript);
 
 const insertion_Sort = (nums) => {
-  for (let i = 1; i < nums.length; i++) {
-    let j = i - 1;
-    let temp = nums[i];
-    while (j >= 0 && nums[j] > temp) {
-      nums[j + 1] = nums[j];
-      j--;
-    }
-    nums[j + 1] = temp;
-  }
-  return nums;
+	for (let i = 1; i < nums.length; i++) {
+		let j = i - 1;
+		let temp = nums[i];
+		while (j >= 0 && nums[j] > temp) {
+			nums[j + 1] = nums[j];
+			j--;
+		}
+		nums[j + 1] = temp;
+	}
+	return nums;
 };
 
 table.addEventListener("click", (e) => {
-  const el = e.target;
-  if (el.nodeName != "TH") {
-    return;
-  }
-  const index = el.cellIndex;
+	const el = e.target;
+	if (el.nodeName != "TH") {
+		return;
+	}
+	const index = el.cellIndex;
 
-  sortTable(index);
+	sortTable(index);
 });
 
 const sortTable = (index) => {
-  const compare = (rowA, rowB) => {
-    return rowB.cells[index].innerHTML - rowA.cells[index].innerHTML;
-  };
-  let rows = [].slice.call(tableBody.rows);
+	const compare = (rowA, rowB) => {
+		return rowB.cells[index].innerHTML - rowA.cells[index].innerHTML;
+	};
+	let rows = [].slice.call(tableBody.rows);
 
-  rows.sort(compare);
-  table.removeChild(tableBody);
-  for (let i = 0; i < rows.length; i++) {
-    tableBody.appendChild(rows[i]);
-  }
+	rows.sort(compare);
+	table.removeChild(tableBody);
+	for (let i = 0; i < rows.length; i++) {
+		tableBody.appendChild(rows[i]);
+	}
 
-  table.appendChild(tableBody);
+	table.appendChild(tableBody);
 };
 
 button.addEventListener("click", function () {
-  sortTable(2);
+	sortTable(2);
 });
